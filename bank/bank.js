3a58bdf2e0f4a1bffe5637c7352ae9d3d3b22da1
Tom Rolfe <tomrolfe20@gmail.com>
2023-02-20 17:57:25 +0000

    replaced balance in tests

--- bank/bank.js
+++ bank/bank.js
@@ -1,30 +1,29 @@
 class Bank {
   constructor() {
     this.statement = [];
-    this.balance = 0;
   }
 
   insert(deposit) {
     this.statement.push(deposit);
-    this.balance += deposit.amount;
   }
 
   withdraw(withdrawal) {
     this.statement.push(withdrawal);
-    this.balance -= withdrawal.amount;
   }
 
   getStatement() {
     let display = 'date || credit || debit || balance';
+    let balance = 0;
     this.statement.forEach((trans) => {
       display += '\n';
       display += trans.date;
       display += ' || ';
-      trans.deposit ? (display += trans.amount) : ' ';
+      trans.deposit ? (display += trans.amount + '.00') : ' ';
       display += ' || ';
-      trans.withdrawal ? (display += trans.amount) : ' ';
+      trans.withdrawal ? (display += trans.amount + '.00') : ' ';
       display += ' || ';
-      display += this.balance;
+      trans.deposit ? (balance += trans.amount) : (balance -= trans.amount);
+      display += balance + '.00';
     });
     return display;
   }
--- integration/integration.test.js
+++ integration/integration.test.js
@@ -17,14 +17,7 @@ describe('Integration', () => {
     bank.insert(deposit2);
     expect(bank.statement).toEqual([deposit1, deposit2]);
   });
-  it('adds 2 deposits and returns a balance', () => {
-    const bank = new Bank();
-    const deposit1 = new Deposit('1/1/1', 1000);
-    const deposit2 = new Deposit('2,2,2', 1000);
-    bank.insert(deposit1);
-    bank.insert(deposit2);
-    expect(bank.balance).toEqual(2000);
-  });
+
   it('adds a deposit and a withdrawal', () => {
     const bank = new Bank();
     const deposit = new Deposit('1/1/1', 1000);
@@ -33,7 +26,7 @@ describe('Integration', () => {
     bank.insert(deposit);
     bank.withdraw(withdrawal);
 
-    expect(bank.balance).toEqual(0);
+    expect(bank.statement).toEqual([deposit, withdrawal]);
   });
   it('adds a deposit and 2 withdrawals', () => {
     const bank = new Bank();
@@ -45,19 +38,29 @@ describe('Integration', () => {
     bank.withdraw(withdrawal1);
     bank.withdraw(withdrawal2);
 
-    expect(bank.balance).toEqual(-1000);
+    expect(bank.statement).toEqual([deposit, withdrawal1, withdrawal2]);
   });
-  it('returns an empty statement when no deposits or withdrawals in correct form', () => {
+  it('returns an empty statement when no deposits or withdrawals in correct format', () => {
     const bank = new Bank();
     expect(bank.getStatement()).toEqual('date || credit || debit || balance');
   });
 
-  it('returns an accurate statement with 1 deposit in correct form', () => {
+  it('returns an accurate statement with 1 deposit in correct format', () => {
     const bank = new Bank();
     const deposit = new Deposit('1/1/1', 1000);
     bank.insert(deposit);
     expect(bank.getStatement()).toEqual(
-      'date || credit || debit || balance\n1/1/1 || 1000 ||  || 1000'
+      'date || credit || debit || balance\n1/1/1 || 1000.00 ||  || 1000.00'
+    );
+  });
+  it('returns an accurate statement with 2 deposits in correct format', () => {
+    const bank = new Bank();
+    const deposit1 = new Deposit('1/1/1', 1000);
+    const deposit2 = new Deposit('1/1/1', 1000);
+    bank.insert(deposit1);
+    bank.insert(deposit2);
+    expect(bank.getStatement()).toEqual(
+      'date || credit || debit || balance\n1/1/1 || 1000.00 ||  || 1000.00\n1/1/1 || 1000.00 ||  || 2000.00'
     );
   });
 });
