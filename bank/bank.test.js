9e539fce64d3a925de347f39095e4ba13cf85db8
Tom Rolfe <tomrolfe20@gmail.com>
2023-02-21 14:19:49 +0000

    added mock withdrawal

--- bank/bank.test.js
+++ bank/bank.test.js
@@ -2,21 +2,34 @@ const Bank = require('./bank.js');
 
 describe('Bank withdrawal app', () => {
   let bank;
+  let mockDeposit1000;
+  let mockDeposit2000;
+  let mockWithdrawal1000;
+
   beforeEach(() => {
     bank = new Bank();
     mockDeposit1000 = { date: '1/1/1', amount: 1000 };
     mockDeposit2000 = { date: '1/1/1', amount: 2000 };
+    mockWithdrawal1000 = { date: '1/1/1', amount: 1000 };
   });
   it('returns an empty array of deposits', () => {
     expect(bank.statement).toEqual([]);
   });
   it('adds a deposit', () => {
     bank.insert(mockDeposit1000);
+
     expect(bank.statement).toEqual([mockDeposit1000]);
   });
   it('adds 2 deposits', () => {
     bank.insert(mockDeposit1000);
     bank.insert(mockDeposit2000);
+
     expect(bank.statement).toEqual([mockDeposit1000, mockDeposit2000]);
+  });
+  it('adds a deposit and a withdrawal', () => {
+    bank.insert(mockDeposit1000);
+    bank.withdraw(mockWithdrawal1000);
+
+    expect(bank.statement).toEqual([mockDeposit1000, mockWithdrawal1000]);
   });
 });
