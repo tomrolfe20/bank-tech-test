2eea1bb64c680e2aef28d6aa32de68f663c56859
Tom Rolfe <tomrolfe20@gmail.com>
2023-02-21 14:18:08 +0000

    added 2 mock deposits

--- bank/bank.test.js
+++ bank/bank.test.js
@@ -5,6 +5,7 @@ describe('Bank withdrawal app', () => {
   beforeEach(() => {
     bank = new Bank();
     mockDeposit1000 = { date: '1/1/1', amount: 1000 };
+    mockDeposit2000 = { date: '1/1/1', amount: 2000 };
   });
   it('returns an empty array of deposits', () => {
     expect(bank.statement).toEqual([]);
@@ -12,5 +13,10 @@ describe('Bank withdrawal app', () => {
   it('adds a deposit', () => {
     bank.insert(mockDeposit1000);
     expect(bank.statement).toEqual([mockDeposit1000]);
+  });
+  it('adds 2 deposits', () => {
+    bank.insert(mockDeposit1000);
+    bank.insert(mockDeposit2000);
+    expect(bank.statement).toEqual([mockDeposit1000, mockDeposit2000]);
   });
 });
